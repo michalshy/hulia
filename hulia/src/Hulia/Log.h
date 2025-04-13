bf7bc7cc9627a737c3ab2eee835bb97349651226
michin <niesmialymichal@gmail.com>
2025-04-13 23:50:29 +0200

    add premake

--- hulia/src/Hulia/Log.h
+++ hulia/src/Hulia/Log.h
@@ -2,8 +2,8 @@
 #include "Core.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
-
 #include <memory>
+
 namespace Hulia {
 	class HULIA_API Log
 	{
@@ -19,18 +19,18 @@ namespace Hulia {
 }
 
 // Logging Macros
-#if defined(_DEBUG)
-	#define HA_CORE_CRITICAL(...)   ::Hulia::Log::GetCoreLogger()->critical(__VA_ARGS__);
-	#define HA_CORE_ERROR(...)      ::Hulia::Log::GetCoreLogger()->error(__VA_ARGS__);
-	#define HA_CORE_WARN(...)       ::Hulia::Log::GetCoreLogger()->warn(__VA_ARGS__);
-	#define HA_CORE_INFO(...)       ::Hulia::Log::GetCoreLogger()->info(__VA_ARGS__);
-	#define HA_CORE_TRACE(...)      ::Hulia::Log::GetCoreLogger()->trace(__VA_ARGS__);
+#if defined(HA_DEBUG)
+	#define HA_CORE_CRITICAL(...) ::Hulia::Log::GetCoreLogger()->critical(__VA_ARGS__);
+	#define HA_CORE_ERROR(...)    ::Hulia::Log::GetCoreLogger()->error(__VA_ARGS__);
+	#define HA_CORE_WARN(...)     ::Hulia::Log::GetCoreLogger()->warn(__VA_ARGS__);
+	#define HA_CORE_INFO(...)     ::Hulia::Log::GetCoreLogger()->info(__VA_ARGS__);
+	#define HA_CORE_TRACE(...)    ::Hulia::Log::GetCoreLogger()->trace(__VA_ARGS__);
 
-	#define HA_CRITICAL(...) ::Hulia::Log::GetClientLogger()->critical(__VA_ARGS__);
-	#define HA_ERROR(...)    ::Hulia::Log::GetClientLogger()->error(__VA_ARGS__);
-	#define HA_WARN(...)     ::Hulia::Log::GetClientLogger()->warn(__VA_ARGS__);
-	#define HA_INFO(...)     ::Hulia::Log::GetClientLogger()->info(__VA_ARGS__);
-	#define HA_TRACE(...)    ::Hulia::Log::GetClientLogger()->trace(__VA_ARGS__);
+	#define HA_CRITICAL(...)      ::Hulia::Log::GetClientLogger()->critical(__VA_ARGS__);
+	#define HA_ERROR(...)         ::Hulia::Log::GetClientLogger()->error(__VA_ARGS__);
+	#define HA_WARN(...)          ::Hulia::Log::GetClientLogger()->warn(__VA_ARGS__);
+	#define HA_INFO(...)          ::Hulia::Log::GetClientLogger()->info(__VA_ARGS__);
+	#define HA_TRACE(...)         ::Hulia::Log::GetClientLogger()->trace(__VA_ARGS__);
 // Striping for release build
 #else
 	#define HA_CORE_CRITICAL(...) 
---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Hulia"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hulia"
+    location "Hulia"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs 
+    {
+        "%{prj.name}/third_party/spdlog/include;"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        buildoptions "/utf-8"
+
+        defines
+        {
+            "HA_PLATFORM_WINDOWS",
+            "HA_BUILD_DLL"
+        }
+
+    filter "configurations:Debug"
+        defines "HA_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "HA_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HA_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs 
+    {
+        "Hulia/third_party/spdlog/include;",
+        "Hulia/src;"
+    }
+
+    links
+    {
+        "Hulia"
+    }
+
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        buildoptions "/utf-8"
+
+        defines
+        {
+            "HA_PLATFORM_WINDOWS"
+        }
+        postbuildcommands
+        {
+          "{COPYFILE} ../bin/" .. outputdir .. "/Hulia/Hulia.dll ../bin/" .. outputdir .. "/Sandbox"
+        }
+
+    filter "configurations:Debug"
+        defines "HA_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "HA_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HA_DIST"
+        optimize "On"
